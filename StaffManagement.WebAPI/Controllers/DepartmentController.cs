4e411f01a86dcc4d60a3b97e555597dfddcf2e78
Dmitry <dolphin1309@mail.com>
2026-04-16 18:56:36 +0700

    Create controllers for departments

---
+++ StaffManagement.WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StaffManagement.Application.Common;
+using StaffManagement.Application.Departments.Commands;
+using StaffManagement.Application.Departments.DTOs;
+using StaffManagement.Application.Departments.Queries;
+
+namespace StaffManagement.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public DepartmentController(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create(CreateDepartmentCommand command, CancellationToken cancellationToken)
+        {
+            var handler = _serviceProvider.GetRequiredService<CreateDepartmentCommandHandler>();
+            var id = await handler.Handle(command, cancellationToken);
+            return Ok(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, UpdateDepartmentDto dto, CancellationToken cancellationToken)
+        {
+            var handler = _serviceProvider.GetRequiredService<UpdateDepartmentCommandHandler>();
+            var command = new UpdateDepartmentCommand(id, dto.Name, dto.Description);
+            await handler.Handle(command, cancellationToken);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+        {
+            var handler = _serviceProvider.GetRequiredService<DeleteDepartmentCommandHandler>();
+            var command = new DeleteDepartmentCommand(id);
+            await handler.Handle(command, cancellationToken);
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DepartmentDto>> GetById(Guid id, CancellationToken cancellationToken)
+        {
+            var handler = _serviceProvider.GetRequiredService<GetDepartmentByIdQueryHandler>();
+            var command = new GetDepartmentByIdQuery(id);
+            var result = await handler.Handle(command, cancellationToken);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PaginatedList<DepartmentDto>>> GetList(
+            [FromQuery] string? searchTerm,
+            [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 10,
+            CancellationToken cancellationToken = default
+            )
+        {
+            var handler = _serviceProvider.GetRequiredService<GetDepartmentListQueryHandler>();
+            var command = new GetDepartmentListQuery(searchTerm, pageNumber, pageSize);
+            var result = await handler.Handle(command, cancellationToken);
+            return Ok(result);
+        }
+    }
+}
--- StaffManagement.WebAPI/Program.cs
+++ StaffManagement.WebAPI/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using StaffManagement.Application.Common.Interfaces;
+using StaffManagement.Application.Departments.Commands;
+using StaffManagement.Application.Departments.Queries;
 using StaffManagement.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,15 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IApplicationDbContext>(provider => 
+provider.GetRequiredService<AppDbContext>());
+
+builder.Services.AddScoped<CreateDepartmentCommandHandler>();
+builder.Services.AddScoped<UpdateDepartmentCommandHandler>();
+builder.Services.AddScoped<DeleteDepartmentCommandHandler>();
+builder.Services.AddScoped<GetDepartmentByIdQueryHandler>();
+builder.Services.AddScoped<GetDepartmentListQueryHandler>();
 
 var app = builder.Build();
 
