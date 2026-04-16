f196c53e50642fd2b5dfd51d9074d125766e9ada
Dmitry <dolphin1309@mail.com>
2026-04-16 19:12:17 +0700

    Created positions dtos

---
+++ StaffManagement.Application/Positions/DTOs/PositionsDTOs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaffManagement.Application.Positions.DTOs
+{
+    public record PositionDto(
+        Guid Id,
+        string Title,
+        decimal? SalaryMin,
+        decimal? Salarymax,
+        Guid DepartmentId,
+        string DepartmentName
+        );
+
+    public record CreatePositionDto(
+        string Title,
+        decimal? SalaryMin,
+        decimal? SalaryMax,
+        Guid DepartmentId
+        );
+
+    public record UpdatePositionDto(
+        string Title,
+        decimal? SalaryMin,
+        decimal? SalaryMax
+        );
+}
