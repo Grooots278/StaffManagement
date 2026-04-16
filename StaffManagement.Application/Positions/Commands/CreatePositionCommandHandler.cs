5165457df9b20b40540e8d576f24909318b7b059
Dmitry <dolphin1309@mail.com>
2026-04-16 19:19:45 +0700

    Implemented command for create position

---
+++ StaffManagement.Application/Positions/Commands/CreatePositionCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffManagement.Application.Common;
+using StaffManagement.Application.Common.Interfaces;
+using StaffManagement.Domain.Entities;
+
+namespace StaffManagement.Application.Positions.Commands
+{
+    public record CreatePositionCommand(string Title, decimal? SalaryMin, decimal? SalaryMax, Guid DepartmentId) : IRequest<Guid>;
+
+    public class CreatePositionCommandHandler
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreatePositionCommandHandler(IApplicationDbContext context) => _context = context;
+
+        public async Task<Guid> Handle(CreatePositionCommand command, CancellationToken cancellationToken)
+        {
+            var department = await _context.Departments
+                .AnyAsync(d => d.Id == command.DepartmentId, cancellationToken);
+
+            if (!department)
+                throw new Exception($"Department with Id {command.DepartmentId} not found");
+
+            var position = new Position(
+                command.Title,
+                command.DepartmentId,
+                command.SalaryMin,
+                command.SalaryMax);
+
+            _context.Positions.Add(position);
+            await _context.SaveChangeAsync(cancellationToken);
+
+            return position.Id;
+        }
+    }
+}
