2b2fcf2dd8c7ae7b61da2344217dd5bd72224b0c
Dmitry <dolphin1309@mail.com>
2026-04-18 11:29:05 +0700

    Creating a MiddleWare for exception handling

---
+++ StaffManagement.WebAPI/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using StaffManagement.Application.Common.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace StaffManagement.WebAPI.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                await HandleExceptionAsync(context, e);   
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode statusCode;
+            object response;
+
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    response = new
+                    {
+                        title = "Validation failed",
+                        status = (int)statusCode,
+                        error = validationException.Errors
+                    };
+                    break;
+
+                case NotFoundException notFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    response = new
+                    {
+                        title = "Resource not found",
+                        status = (int)statusCode,
+                        detail = notFoundException.Message
+                    };
+                    break;
+
+                default:
+                    _logger.LogError(exception, "Unhandled exception");
+                    statusCode = HttpStatusCode.InternalServerError;
+                    response = new
+                    {
+                        title = "An error occurred"
+                    };
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response, jsonOptions));
+        }
+    }
+}
