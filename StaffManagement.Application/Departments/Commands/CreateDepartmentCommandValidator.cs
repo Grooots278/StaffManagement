68df5b2bd91da3e32759c00e4e57a61cf89a7014
Dmitry <dolphin1309@mail.com>
2026-04-18 10:35:51 +0700

    Adding a validators for departments commands

---
+++ StaffManagement.Application/Departments/Commands/CreateDepartmentCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace StaffManagement.Application.Departments.Commands
+{
+    public class CreateDepartmentCommandValidator : AbstractValidator<CreateDepartmentCommand>
+    {
+        public CreateDepartmentCommandValidator() 
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Department name is required.")
+                .MaximumLength(100).WithMessage("Department name must not exceed 100 characters.");
+        }
+    }
+}
---
+++ StaffManagement.Application/Departments/Commands/UpdateDepartmentCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace StaffManagement.Application.Departments.Commands
+{
+    public class UpdateDepartmentCommandValidator : AbstractValidator<UpdateDepartmentCommand>
+    {
+
+        public UpdateDepartmentCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Department name is required.")
+                .MaximumLength(100);
+        }
+    }
+}
