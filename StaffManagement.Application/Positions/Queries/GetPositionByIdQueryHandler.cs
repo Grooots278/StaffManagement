885ebf38c7c8b69d7915847217dbd7552a92d573
Dmitry <dolphin1309@mail.com>
2026-04-18 10:02:09 +0700

    Implemented a query to output a position by id

---
+++ StaffManagement.Application/Positions/Queries/GetPositionByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffManagement.Application.Common;
+using StaffManagement.Application.Common.Interfaces;
+using StaffManagement.Application.Positions.DTOs;
+
+namespace StaffManagement.Application.Positions.Queries
+{
+
+    public record GetPositionByIdQuery(Guid Id) : IRequest<PositionDto?>;
+
+    public class GetPositionByIdQueryHandler
+    {
+
+        private readonly IApplicationDbContext _context;
+
+        public GetPositionByIdQueryHandler(IApplicationDbContext context) => _context = context;
+
+        public async Task<PositionDto?> Handle(GetPositionByIdQuery query, CancellationToken cancellationToken)
+        {
+            var position = await _context.Positions
+                .Include(d => d.Department)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(i => i.Id == query.Id);
+
+            if (position == null) return null;
+
+            return new PositionDto
+            (
+                position.Id, position.Title, position.MinSalary, position.MaxSalary, position.DepartmentId, position.Department.Name
+            );
+        }
+    }
+}
