e3622d74c21fe48ab3a3510f68f530e544c04b34
Dmitry <dolphin1309@mail.com>
2026-04-18 11:12:43 +0700

    Create folder to custom exceptions

---
+++ StaffManagement.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,19 @@
+﻿namespace StaffManagement.Application.Common.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IDictionary<string, string[]> Errors { get; set; }
+
+        public ValidationException() : base() 
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<FluentValidation.Results.ValidationFailure> failure) : this()
+        {
+            Errors = failure
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failuregroup => failuregroup.Key, failuregroup => failuregroup.ToArray());
+        }
+    }
+}
