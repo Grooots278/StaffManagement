dfb90bfc85190c3b5aaa4a1a07a02b1eee365fae
Dmitry <dolphin1309@mail.com>
2026-04-19 11:07:08 +0700

    Adding Employee in AppDbContext

--- StaffManagement.Infrastructure/Data/AppDbContext.cs
+++ StaffManagement.Infrastructure/Data/AppDbContext.cs
@@ -11,6 +11,7 @@ namespace StaffManagement.Infrastructure.Data
 
         public DbSet<Department> Departments => Set<Department>();
         public DbSet<Position> Positions => Set<Position>();
+        public DbSet<Employee> Employees => Set<Employee>();
 
         public Task<int> SaveChangeAsync(CancellationToken cancellationToken)
         {
@@ -41,6 +42,32 @@ namespace StaffManagement.Infrastructure.Data
 
                 entity.HasIndex(p => new { p.Title, p.DepartmentId }).IsUnique();
             });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.LastName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.MiddleName).HasMaxLength(50);
+
+                entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+                entity.HasIndex(e => e.Email).IsUnique();
+
+                entity.Property(e => e.Phone).HasMaxLength(20);
+                entity.Property(e => e.Salary).HasPrecision(18, 2);
+
+                entity.HasOne(e => e.Department)
+                .WithMany()
+                .HasForeignKey(e => e.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(e => e.Position)
+                .WithMany()
+                .HasForeignKey(e => e.PositionId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
         }
 
     }
