88720d5e810529818c11004a1a0f3bda8ea44c4b
Dmitry <dolphin1309@mail.com>
2026-05-03 12:22:01 +0700

    Adding deletes command to Employee

---
+++ StaffManagement.Application/Employees/Commands/DeleteEmployeeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using StaffManagement.Application.Common.Exceptions;
+using StaffManagement.Application.Common.Interfaces;
+
+namespace StaffManagement.Application.Employees.Commands
+{
+    public record DeleteEmployeeCommand(Guid Id) : IRequest<Unit>;
+    public class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand, Unit>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteEmployeeCommandHandler(IApplicationDbContext context) => _context = context;
+
+        public async Task<Unit> Handle(DeleteEmployeeCommand command, CancellationToken cancellationToken)
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(i => i.Id == command.Id, cancellationToken);
+
+            if (employee == null)
+                throw new NotFoundException($"Employee with Id {command.Id} not found.");
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangeAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
