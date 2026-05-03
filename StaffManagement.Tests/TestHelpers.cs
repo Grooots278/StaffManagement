ea7af505003707fa5a6987388a4aebd6c153cd39
Dmitry <dolphin1309@mail.com>
2026-05-03 23:12:01 +0700

    Create class for changes Id

---
+++ StaffManagement.Tests/TestHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace StaffManagement.Tests
+{
+    public static class TestHelpers
+    {
+        public static void SetId<T>(T entity, Guid id)
+        {
+            var property = typeof(T).GetProperty("Id",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            if (property == null)
+                throw new InvalidOperationException($"Property 'Id' not found on {typeof(T).Name}");
+
+            property.SetValue(entity, id);
+        }
+    }
+}
