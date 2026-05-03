db1c7f06614443808de1f4636cb66589424e6333
Dmitry <dolphin1309@mail.com>
2026-05-03 12:49:48 +0700

    Adding validator to create employee command

---
+++ StaffManagement.Application/Employees/Commands/CreateEmployeeCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace StaffManagement.Application.Employees.Commands
+{
+    public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+    {
+        public CreateEmployeeCommandValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.LastName).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.MiddleName).MaximumLength(50);
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().MaximumLength(100);
+            RuleFor(x => x.Phone).MaximumLength(20).Matches(@"^[\d\s\+\-\(\)]+$").When(x => !string.IsNullOrEmpty(x.Phone));
+            RuleFor(x => x.HireDate).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Hire date cannot be in the future.");
+            RuleFor(x => x.Salary).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.DepartmentId).NotEmpty();
+            RuleFor(x => x.PositionId).NotEmpty();
+        }
+    }
+}
